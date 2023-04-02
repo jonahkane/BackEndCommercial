a47c6267216eb69ad29fed89de9b58673e83ce65
Jonah <jonahtkane@gmail.com>
2023-04-02 14:31:28 -0500

    updated references in the project.js model

--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,9 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
 Product.init(
   {
     id: {
@@ -22,17 +19,24 @@ Product.init(
       price: {
         type: DataTypes.DECIMAL,
         allowNull: false,
-        // validates that value is decimal???
+        validate: {
+          isDecimal: true,
+        }
       },
       stock: {
         type: DataTypes.INTEGER,
         allowNull: false,
         defaultValue: 10,
-        //validates that balue is numeric
+        validate: {
+          isNumeric: true,
+        }
       },
       category_id: {
         type: DataTypes.INTEGER,
-        //References the category model's id
+        references: {
+          model: 'category',
+          key: 'id',
+        },
       },
     
   },
