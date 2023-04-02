ada980665dfd967019e04f127dbae72a9d41e663
Jonah <jonahtkane@gmail.com>
2023-04-01 21:21:44 -0500

    updated category routes .js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,57 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-});
+router.get('/', async (req, res) => {
+  Category.findAll({
+    include: [Product]
+  }).then((data) => {
+    res.json(data);
+  });
+  });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    include: [Product],
+    where: {
+      id: req.params.id
+    }
+  }).then((data) => {
+    res.json(data);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+}).then((data) => {
+  res.json(data);
+
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then((data) => {
+    res.json(data);
+  });
+
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then((data) => {
+    res.json(data);
+  });
+
 });
 
 module.exports = router;
