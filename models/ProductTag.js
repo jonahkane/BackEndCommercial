a25c608ac28a9163b0be45cfa4d077163575373b
Jonah <jonahtkane@gmail.com>
2023-04-02 14:33:21 -0500

    updated references in the productTag.js model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -14,11 +14,17 @@ ProductTag.init(
     },
     product_id: {
       type: DataTypes.INTEGER,
-      //references procut models id
+      references: {
+        model: 'product',
+        key: 'id',
+      },
     },
     tag_id: {
       type: DataTypes.INTEGER,
-      //REFERENES THE TAG MODELS ID
+      references: {
+        model: 'tag',
+        key: 'id',
+      }
     },
   },
   {
