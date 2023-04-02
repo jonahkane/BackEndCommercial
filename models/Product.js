0b0ce0637feca0a290bed265f97619c450d98cd9
Jonah <jonahtkane@gmail.com>
2023-04-01 20:39:07 -0500

    added SOME code to product js model.... not done.

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,32 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+        },
+      product_name:{
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        // validates that value is decimal???
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10,
+        //validates that balue is numeric
+      },
+      category_id: {
+        type: DataTypes.INTEGER,
+        //References the category model's id
+      },
+    
   },
   {
     sequelize,
